6bf63d5c0d210711c0fe37f89b1a68c69192e416
Chirag Pandey <pndy.chrg2003@gmail.com>
2022-02-24 17:32:11 +0530

    created loading widget

---
+++ lib/shared/loading.dart
@@ -0,0 +1,19 @@
+import 'package:contest_app/shared/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: SpinKitSpinningLines(
+          color: kpurple,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
