f0c5fb75147a6df8cc092be553b5931dc930b824
Chirag Pandey <pndy.chrg2003@gmail.com>
2022-02-27 15:40:06 +0530

    created version 1.0.0+.2
    - Changed Launcher Icon
    - Added Scrolling feature on Startup Screen for devices with different resolution

--- lib/screens/startup_screen/startup.dart
+++ lib/screens/startup_screen/startup.dart
@@ -30,128 +30,130 @@ class StartUP extends StatelessWidget {
       //   backgroundColor: kLightPink,
       //   title: Center(child: Text("Contest App")),
       // ),
-      body: Container(
-        padding: EdgeInsets.only(top: 50),
-        child: Column(
-          children: <Widget>[
-            CarouselSlider(
-              options: CarouselOptions(height: 570.0),
-              items: [1, 2, 3, 4].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        width: MediaQuery.of(context).size.width,
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 5.0, vertical: 10),
-                        decoration: BoxDecoration(
-                          color: MediaQuery.of(context).platformBrightness ==
-                                  Brightness.light
-                              ? Colors.white
-                              : kdarkBlue,
-                          border: Border.all(
-                            color: kLightPink,
-                            width: 4,
-                          ),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              carouselText[i],
-                              style: GoogleFonts.lato(
-                                color:
-                                    MediaQuery.of(context).platformBrightness ==
-                                            Brightness.light
-                                        ? Colors.black
-                                        : Colors.white,
-                                fontSize: 30,
-                              ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(top: 50),
+          child: Column(
+            children: <Widget>[
+              CarouselSlider(
+                options: CarouselOptions(height: 570.0),
+                items: [1, 2, 3, 4].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 5.0, vertical: 10),
+                          decoration: BoxDecoration(
+                            color: MediaQuery.of(context).platformBrightness ==
+                                    Brightness.light
+                                ? Colors.white
+                                : kdarkBlue,
+                            border: Border.all(
+                              color: kLightPink,
+                              width: 4,
                             ),
-                            Container(
-                              height: 300,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                      "assets/images/${i.toString()}.png"),
-                                  fit: BoxFit.cover,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                carouselText[i],
+                                style: GoogleFonts.lato(
+                                  color: MediaQuery.of(context)
+                                              .platformBrightness ==
+                                          Brightness.light
+                                      ? Colors.black
+                                      : Colors.white,
+                                  fontSize: 30,
                                 ),
                               ),
-                            )
-                          ],
-                        ));
+                              Container(
+                                height: 300,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "assets/images/${i.toString()}.png"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ));
+                    },
+                  );
+                }).toList(),
+              ),
+              Text(
+                "If You haven't Registered Yet",
+                style: GoogleFonts.lato(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(8),
+                    primary: kLightPink,
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: ((context) => SignUp())));
                   },
-                );
-              }).toList(),
-            ),
-            Text(
-              "If You haven't Registered Yet",
-              style: GoogleFonts.lato(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(8),
-                  primary: kLightPink,
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: ((context) => SignUp())));
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Icon(FontAwesomeIcons.signInAlt),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Sign Up",
-                    ),
-                  ],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.signInAlt),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Sign Up",
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Already Registered",
-              style: GoogleFonts.lato(
-                fontSize: 15,
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(8),
-                  primary: kLightPurple,
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: ((context) => SignIn())));
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Icon(FontAwesomeIcons.personBooth),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "Sign In",
-                    ),
-                  ],
+              Text(
+                "Already Registered",
+                style: GoogleFonts.lato(
+                  fontSize: 15,
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(8),
+                    primary: kLightPurple,
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: ((context) => SignIn())));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.personBooth),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Sign In",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
